ac7adb90c951569f890835d5422b4003a7f810d4
Emrie <emrie@emries-mbp.mynetworksettings.com>
2023-06-14 23:59:28 -0400

    added the routes

--- api/server.js
+++ api/server.js
@@ -1,1 +1,24 @@
 // build your server here and require it from index.js
+const express = require("express")
+const server = express()
+
+
+server.use(express.json())
+
+const projectRouter = require('./project/router') //this goes under the server
+const resourceRouter = require('./resource/router')
+const taskRouter = require('./project/router')
+
+
+//this connects the node server to the express routing system.
+server.use('/api/project' , projectRouter) 
+server.use('/api/resource' , resourceRouter)
+server.use('/api/task' , taskRouter) 
+
+
+// DO YOUR MAGIC
+server.use('*', (req, res, next)=>{
+next({status: 404, message: 'not found'})
+})
+
+module.exports = server
